305e539bc19566f54d8af9059bc4f407819a2b27
Aryan Chandra <aryanprofessional498@gmail.com>
2025-05-07 17:22:04 +0530

    some padai

---
+++ code/data_types.py
@@ -0,0 +1,34 @@
+# 	Numbers (integers, floats, complex)
+
+a = 241 #integer
+b = 89.80 #float
+c = 141 + 41j #complex
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# 	Strings (immutability✅, methods✅)
+
+name = 'aadi'
+name = "chaudry aafi"
+name = "hello my name is aadi"
+print(type(name))
+print(name.upper())
+print(name.split())
+print(name.replace("aadi", "vivan"))
+
+# methods: pre-build functions in python
+
+
+# 	Booleans✅
+
+a = True
+b = False
+print(type(a), type(b))
+
+
+# 	Lists (mutability, methods, list comprehensions)
+# 	Tuples (immutability)
+# 	Dictionaries (key-value pairs, methods)
+# 	Sets (uniqueness, methods)
---
+++ code/print_statement.py
@@ -0,0 +1,4 @@
+print(424)
+print("hello world!")
+
+# this is a print statement in python, we use print() to give output through terminal
